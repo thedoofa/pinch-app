ae6502ee494c7ae85af340f77b74ba4a48fa1df9
thedoofa <kay@Kays-Macbook-Pro.local>
2016-05-27 03:01:27 +0100

    push notifications

--- js/index.js
+++ js/index.js
@@ -46,4 +46,22 @@ var app = {
 
         console.log('Received Event: ' + id);
     }
+    // Add to index.js or the first page that loads with your app.
+// For Intel XDK and please add this to your app.js.
+
+document.addEventListener('deviceready', function () {
+  // Enable to debug issues.
+  // window.plugins.OneSignal.setLogLevel({logLevel: 4, visualLevel: 4});
+  
+  var notificationOpenedCallback = function(jsonData) {
+    console.log('didReceiveRemoteNotificationCallBack: ' + JSON.stringify(jsonData));
+  };
+
+  window.plugins.OneSignal.init("cfabfc7d-2768-428a-8afd-5e71b14262eb",
+                                 {googleProjectNumber: "725010211965"},
+                                 notificationOpenedCallback);
+  
+  // Show an alert box if a notification comes in when the user is in your app.
+  window.plugins.OneSignal.enableInAppAlertNotification(true);
+}, false);
 };
