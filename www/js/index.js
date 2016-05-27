83e17b82bcb1462e0aabd6591289593115d16cbd
thedoofa <kay@Kays-Macbook-Pro.local>
2016-05-27 03:14:41 +0100

    enable debug

--- www/js/index.js
+++ www/js/index.js
@@ -29,8 +29,8 @@ var app = {
         document.addEventListener('deviceready', this.onDeviceReady, false);
     },
     	document.addEventListener('deviceready', function () {
-  // Enable to debug issues.
-  // window.plugins.OneSignal.setLogLevel({logLevel: 4, visualLevel: 4});
+
+window.plugins.OneSignal.setLogLevel({logLevel: 4, visualLevel: 4});
   
   var notificationOpenedCallback = function(jsonData) {
     console.log('didReceiveRemoteNotificationCallBack: ' + JSON.stringify(jsonData));
